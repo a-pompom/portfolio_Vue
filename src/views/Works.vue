a8a10a55a0ddb2dabaf6eaf199a6600e1a678117
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 19:49:31 +0900

    外部ファイルのJSONをもとにつくったもの一覧を表示できるよう修正

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -8,111 +8,30 @@
 		<div class="works__content">
 			
 			<ul class="works__content-list">
-			
-				<li class="works__content-list__item">
-				
-					<a href="https://github.com/a-pompom/spring-boot-twitter_clone_practice">
-						<div class="item__header">
-							<img src="../assets/app_capture/tweet_app.png">
-							<h3>Twitterクローンアプリ</h3>
-						</div>
-						
-						<ul class="item__icon-list">
-							<li class="item__icon-list__image">
-								<h2>Spring</h2>		
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/java.svg">
-								<p>Java</p>
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/javascript.svg">
-								<p>JavaScript</p>
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/vue-dot-js.svg">
-								<p>Vue.js</p>
-							</li>
-						</ul>
-						
-					</a>
+				<li 
+					v-for="(workSummary, index) in worksSummary"
+					v-bind:key="workSummary.id"
+					class="works__content-list__item">
 					
-				</li>
-				
-				<li class="works__content-list__item">
-				
-					<a href="https://github.com/a-pompom/vue-markdown-editor">
-						<div class="item__header">
-							<img src="../assets/app_capture/markdown_app.png">
-							<h3>Markdownエディタっぽいやつ</h3>
-						</div>
-						
-						<ul class="item__icon-list">
-							<li class="item__icon-list__image">
-								<img src="../assets/external/javascript.svg">
-								<p>JavaScript</p>
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/vue-dot-js.svg">
-								<p>Vue.js</p>
-							</li>
-						</ul>
-						
-					</a>
+					<div class="item__header">
+						<img v-bind:src="workSummary.appImageSource">
+						<h3>{{ workSummary.appName }}</h3>
+					</div>
+					<ul class="item__icon-list">
 					
+						<li 
+							v-for="(itemIcon, index) in workSummary.appIconList"
+							v-bind:key="itemIcon.id"
+							class="item__icon-list__image">
+							
+							<h2 v-if="itemIcon.imgSource === '' ">{{ itemIcon.tagName }}</h2>
+							<img v-else v-bind:src="itemIcon.imgSource">
+							
+							<p>{{ itemIcon.tagName }}</p>
+						</li>
+						
+					</ul>
 				</li>
-				
-				<li class="works__content-list__item">
-				
-					<a href="https://github.com/a-pompom/wg/tree/master/kintone_report">
-						<div class="item__header">
-							<img src="../assets/app_capture/kintone_report_app.png">
-							<h3>kintoneを利用して日報っぽい文章を作成するエディタ</h3>
-						</div>
-						
-						<ul class="item__icon-list">
-							<li class="item__icon-list__image">
-								<h4>Kintone</h4>
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/javascript.svg">
-								<p>JavaScript</p>
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/vue-dot-js.svg">
-								<p>Vue.js</p>
-							</li>
-						</ul>
-						
-						
-					</a>
-					
-				</li>
-				
-				<li class="works__content-list__item">
-				
-					<a href="https://github.com/a-pompom/spring-boot-login_app_demo">
-						<div class="item__header">
-							<img src="../assets/app_capture/springboot-security.png">
-							<h3>SpringBoot + SpringSecurityでログイン機能の作成</h3>
-						</div>
-						
-						<ul class="item__icon-list">
-							<li class="item__icon-list__image">
-								<h2>Spring</h2>		
-							</li>
-							<li class="item__icon-list__image">
-								<img src="../assets/external/java.svg">
-								<p>Java</p>
-							</li>
-						</ul>
-					</a>
-					
-				</li>
-				
-				
-				
-				
 			</ul>
 			
 		</div>
@@ -127,7 +46,9 @@
 	export default {
 		
 		data() {
-			worksSummary: worksSummary
+			return {
+				worksSummary: worksSummary
+			}
 		}
 		
 	}
