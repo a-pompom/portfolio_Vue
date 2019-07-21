a1c1cae0c6efbf42028d09e1d567b8388b625fa9
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 21:02:54 +0900

    モーダル用のコンポーネントをとりあえず作成

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -18,6 +18,11 @@
 						<img v-bind:src="workSummary.appImageSource">
 						<h3>{{ workSummary.appName }}</h3>
 					</div>
+					<works-detail-modal
+						v-bind:visible="modalVisibleArray[index]"
+						v-bind:index="index"
+						v-bind:workDetail="worksDetail[index]">
+					</works-detail-modal>
 					<ul class="item__icon-list">
 					
 						<li 
@@ -29,6 +34,7 @@
 							<img v-else v-bind:src="itemIcon.imgSource">
 							
 							<p>{{ itemIcon.tagName }}</p>
+							
 						</li>
 						
 					</ul>
@@ -36,6 +42,8 @@
 			</ul>
 			
 		</div>
+		
+		
 	
 	</div>
 </template>
@@ -43,14 +51,21 @@
 <script>
 	
 	import worksSummary from "../assets/works_data/works.json";
+	import worksDetailModal from "./WorksDetailModal.vue";
+	import worksDetail from "../assets/works_data/worksDetail.json";
 	
 	export default {
 		
 		data() {
 			return {
 				worksSummary: worksSummary,
-				isModalVisible: false
+				worksDetail: worksDetail,
+				modalVisibleArray: []
 			};
+		},
+		
+		components: {
+			worksDetailModal: worksDetailModal
 		},
 		
 		methods: {
@@ -62,8 +77,16 @@
 				}
 				
 				console.log("called by " + index);
-				this.isModalVisible = true;
+				this.modalVisibleArray[index] = false;
+				console.log(this.modalVisibleArray);
 			}
+		},
+		
+		mounted() {
+			for (let i= 0; i < this.worksSummary.length; i++) {
+				this.modalVisibleArray.push(true);
+			}
+			console.log(this.modalVisibleArray);
 		}
 		
 	}
---
+++ src/views/WorksDetailModal.vue
@@ -0,0 +1,30 @@
+<template>
+	<div v-show="visible" class="works__modal">	
+		<p>{{ workDetail.gitHubLink }}</p>
+	</div>
+</template>
+
+
+<script>
+	
+	
+	export default {
+		props: {
+			visible: Boolean,
+			index: Number,
+			workDetail: Object
+		},
+		
+		watch: {
+			visible(value) {
+				console.log("visible changed");
+			}
+		},
+		
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
