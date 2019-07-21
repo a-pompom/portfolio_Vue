c824dbbf081c0be314097fbf992980c007871e20
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 13:59:56 +0900

    ヘッダ部分とりあえず完成 close #2

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,21 @@
 	<div id="app">
 		<div id="nav" class="header">
 		
-			<ul>
-				<li>
+			<ul class="header--nav-list">
+				<li class="header--nav-list__item">
 					<router-link to="/">Home</router-link>
 				</li>
-				<li>
+				<li class="header--nav-list__item">
 					<router-link to="/profile">Profile</router-link>
 				</li>
-				<li>
+				<li class="header--nav-list__item">
 					<router-link to="/works">Works</router-link>
 				</li>
-				<li>
+				<li class="header--nav-list__item">
 					<router-link to="/social">Social</router-link>
 				</li>
 			</ul>
 	
-			
-			
-			
-			
-
 		</div>
 		
 		<router-view/>
@@ -29,5 +24,27 @@
 </template>
 
 <style lang="scss">
-
+	
+	.header {
+		
+		width: 80%;
+		margin: 5px auto;
+		
+		&--nav-list {
+			
+			@include flex-between;
+			
+			&__item {
+				
+				font-size: 2.6rem;
+				border-bottom: 1px solid #4d464f;
+				
+				&:hover {
+					background-color: #f8f8f8;
+				}
+			}
+		}
+		
+	}
+	
 </style>
