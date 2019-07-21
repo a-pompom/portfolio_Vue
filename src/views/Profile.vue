cf3c33546db131becef24d184803d9cbff6d0c5d
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 14:26:31 +0900

    プロフィール画面マークアップ #3

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="profile">
-    <h1>This is an profile page</h1>
-  </div>
+	<div class="profile">
+	
+		<div class="profile__title">
+			<h1>プロフィール</h1>
+		</div>
+		
+		<div class="profile__content">
+			
+			<div class="profile__content--image">
+				<img src="../assets/profile.jpg">
+				<img src="../assets/profile_2.png">
+			</div>
+			
+			<div class="profile__content--text">
+				<h3>名前: a-pompom</h3>
+				<p>pompomは隣の黄色い彼からきています。</p>
+				<h3>好きな言語: JavaScript</h3>
+				<p>もう少し仲良くなりたい</p>
+				
+				<hr>
+				
+				<h3>趣味:</h3>
+				<h5>読書</h5>
+				<p>技術書から小説まで何でも読みます。脳内ファンタジア。</p>
+				<h5>英語</h5>
+				<p>学生時代にもっと頑張っておけばよかった人生。</p>
+				<p>PodCastが通勤のおともです。</p>
+				<h5>お絵描き</h5>
+				<p>同人誌を出せば在庫の達人になる程度。</p>
+				<p>もっと頑張りたい。</p>
+			</div>
+			
+		</div>	
+	</div>
 </template>
+
+
+<style lang="scss" scoped>
+</style>
