118aaefd2642e26966e989dc13580d7dbf249e2a
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 11:04:02 +0900

    とりあえずマークアップ #1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
- 	<div class="home" style="margin: auto auto;">
- 		<img src="../assets/home.png">
+	<div class="top">
+	
+		<div class="top__title">
+			<h1>(ポム) * (ポム)</h1>
+		</div>
+
+		<div class="top__image">
+			<img src="../assets/home.png">
+		</div>
+		
+		<div class="top__icon-list">
+
+		</div>
+		
 	</div>
+	
 </template>
 
 <script>
