5647f0e2a8db449e74f07f770a353ab4b33df969
a-pompom <aoi.matsuda.3x@gmail.com>
2019-07-21 14:59:12 +0900

    Social画面をマークアップ #4

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="social">
-    <h1>This is an social page</h1>
-  </div>
+	<div class="social">
+		
+		<div class="social__title">
+			<h1>活動場所</h1>
+		</div>
+		
+		<div class="social__content">
+			
+			<ul class="social__content-list">
+				<li class="social__content-list__item">
+					<div class="item__header">
+						<img src="../assets/external/github.svg">
+						<h3>GitHub</h3>
+					</div>
+					<p>
+						色々と作ったものを置いてます。まだまだ数が少ないからもっと頑張りたい。
+					</p>
+				</li>
+				<li class="social__content-list__item">
+					<div class="item__header">
+						<img src="../assets/external/qiita.svg">
+						<h3>Qiita</h3>
+					</div>
+					<p>
+						たまーにきまぐれで投稿しています。もう少し投稿増やしたい。
+					</p>
+				</li>
+				<li class="social__content-list__item">
+					<div class="item__header">
+						<img src="../assets/external/twitter.svg">
+						<h3>Twitter</h3>
+					</div>
+					<p>
+						技術的なこととか呟いたり呟かなかったり。
+					</p>
+				</li>
+			</ul>
+			
+		</div>
+	
+	</div>
 </template>
